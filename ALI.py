005a7bd11d4f5ccc03fd8eee634a9d74ccdd7427
FewerElk <120582916+FewerElk@users.noreply.github.com>
2023-07-03 19:25:57 +0200

    Update ALI.py

--- ALI.py
+++ ALI.py
@@ -9,9 +9,32 @@ except ModuleNotFoundError:
 #Class
 
 class ALI(object):
-    def __init__(self):
+    def __init__(self, lib:str, log=True):
+        self.log = log
+        self.lib = lib
+        
+    def auto(self):
+        """return the imported lib"""
+        if self.check_installed():
+            return self.import()
+        else:
+            self.install()
+            return self.import
+        
+    def check_installed(self):
+        """check if the lib is installed"""
         ...
         
-    def import(self, lib:str):
-        """import a lib"""
+    def import(self):
+        """import the lib"""
+        with open("a.py, "w") as temp:
+            temp.write("""TEMP FILE OF ALI
+import {0} as l
+def ret():
+    return l"""
+        import a
+        return a.ret()
+        
+    def install(self):
+        """install the lib"""
         ...
