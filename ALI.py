d5a9ac3037ad5ca887bec2beb6f1c4d09d9deb1f
FewerElk <120582916+FewerElk@users.noreply.github.com>
2023-07-03 18:53:34 +0200

    Update ALI.py

--- ALI.py
+++ ALI.py
@@ -1,1 +1,17 @@
+"""This file contain the class for this lib"""
 
+try:
+    import os
+except ModuleNotFoundError:
+    print("You need the os librairie !")
+    exit(-1)
+    
+#Class
+
+class ALI(object):
+    def __init__(self):
+        ...
+        
+    def import(self, lib:str):
+        """import a lib"""
+        ...
